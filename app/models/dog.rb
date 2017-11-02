4fe95401dfdaf4e48ee8d930be9b9aa1b4f43141
Lisa Lee <snickers495@gmail.com>
2017-11-02 16:39:43 +0000

    automatically backed up by learn

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -3,9 +3,9 @@ class Dog
   attr_accessor :name, :age
   attr_reader :breed
   def initialize(name, age, breed)
-    @name = name 
+    @name = name
     @age = age
     @breed = breed
 
-  end 
+  end
 end
